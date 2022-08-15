8b9c1b28321a426ffa4abaf5bc432624ff6ebe16
Daniel S <DanielSoDev@yandex.ru>
2022-08-16 01:34:11 +0200

    feat: реализовал FileBackedTasksManager

--- src/Main.java
+++ src/Main.java
@@ -6,51 +6,6 @@ import Task.SubTask;
 import Task.Task;
 
 //public class Main {
-/*
-
-    Способ чтения : Files.readString(Path.of(path));
-
-   4. Исключения вида IOException нужно отлавливать внутри метода save и кидать собственное
-    непроверяемое исключение ManagerSaveException (класс с исключением нужно создать)
-
-   5. Создать статический метод в FileBackedTasksManager static loadFromFile(File file),
-    который будет восстанавливать данные менеджера из файла при запуске программы.
-
-
-  1.  если айдишник назначается в  addTask - то это пробелма.
-сложно скзаать, не видя код, но мне кажется, самое просто будет, это проверять - если айдишник есть у таски,
- то не присваивать его. Это костыль,
-2.
-static String historyToString(HistoryManager manager
-этот метод просто выбирает все айдишники из истории и записывает их в строку.
-типа, у нас были таски в истории с айдишниками 1,2,3
-вот это тметод превартил этот менеджер истории в строчку
-"1,2,3"
-static List<Integer> historyFromString(String value)
-сделает обратно.  потом по этим айдишникам надо найти таски с этими айдишниками и сохранить их в historyManager
-т.е. что то вроде
-... достаем из файла сначала таски и сохраняем в менеджер.
-потом по сроке "1,2,3"
-taskManager.historyManager.add(taskManager.findTask(taskId));
-идея, я думаю понятна.
-3.
-"мне не понятно, как я могу восстановить данные менеджера,
- запуская static FileBackedTasksManager loadFromFile(File file)"
-тут важный момент - менеджер сохраняет таски в файлы только по операции get.
-Поэтому здесь делаем так -
-создаем новый менеджер
-public static FileBackedTaskManager loadFromFile(File file) {
-   final FileBackedTaskManager taskManager = new FileBackedTaskManager(file);
-далее считываем таски и
-final Task task = читаем из файла строчку
-taskManager.addTask(task);
-считываем из файла таск.
-"+не могу в нем запускать методы по добавлению тасков"
-можешь, это же статический метод  и у тебя есть экземпляр вновь созданного менеджера -вот на нем и надо вызывать методы
- */
-
-
-
 
    /* public static void main(String[] args) {
 
